421b24f172ccdcfb5a136010e232183577d6580c
Dennis Baum <dennis.baum@smail.inf.h-brs.de>
2020-09-07 13:49:26 +0200

    Created test class for CarControl

---
+++ src/test/java/org/bonn/se/carlook/process/control/CarControlTest.java
@@ -0,0 +1,28 @@
+package org.bonn.se.carlook.process.control;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarControlTest {
+
+    @Test
+    void reserveCar() {
+    }
+
+    @Test
+    void addNewCar() {
+    }
+
+    @Test
+    void getInsertedCarsFromUser() {
+    }
+
+    @Test
+    void getReservedCarsFromUser() {
+    }
+
+    @Test
+    void getAllCars() {
+    }
+}
